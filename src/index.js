feeaca28d50f5a2b31ba00535d134ee14c002ff2
Dion Low <dionlow17@gmail.com>
2019-06-10 16:34:10 -0700

    Creating react-redux repo for start coding challenges

---
+++ src/index.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './sass/main.scss';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+import { BrowserRouter } from 'react-router-dom'
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
+
+
+// Redux Extension added
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+// Reducers
+const rootReducers = combineReducers({
+    
+})
+
+const store = createStore(rootReducers, composeEnhancers(
+    applyMiddleware(thunk)
+))
+
+const app = (
+    <Provider store={store}>
+        <BrowserRouter>        
+            <App />
+        </BrowserRouter>
+    </Provider>    
+)
+
+
+ReactDOM.render(app, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
